3416f3453a7cf1194e0aec1c28419903bbafbf67
philip king <144460960+philippkk@users.noreply.github.com>
2025-05-05 18:04:28 -0500

    in a good spot

---
+++ 437tutorial/Assets/scripts/spiderController.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spiderController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    [Header("Head Tracking")]
+    [SerializeField] Transform headBone;
+    [SerializeField] float headMaxTurnAngle = 70f;
+    [SerializeField] float headTrackingSpeed = 10f;
+    
+    [Header("Root Motion")]
+    [SerializeField] float turnSpeed = 100f;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float turnAcceleration = 5f;
+    [SerializeField] float moveAcceleration = 5f;
+    [SerializeField] float minDistToTarget = 4.5f;
+    [SerializeField] float maxDistToTarget = 6f;
+    [SerializeField] float maxAngToTarget = 25f;
+
+    Vector3 currentVelocity;
+    float currentAngularVelocity;
+
+    [Header("Legs")]
+    [SerializeField] LegStepper_Simple frontLeftLegStepper;
+    [SerializeField] LegStepper_Simple frontRightLegStepper;
+    [SerializeField] LegStepper_Simple backLeftLegStepper;
+    [SerializeField] LegStepper_Simple backRightLegStepper;
+
+    void Awake()
+    {
+        StartCoroutine(LegUpdateCoroutine());
+    }
+
+    void LateUpdate()
+    {
+        // Update order is important! 
+        // We update things ordered by dependencies. Since the eyes rely on the head, and the head relies
+        // on the body, we update the body first, then the head, and finally the eyes.
+
+        RootMotionUpdate();
+        HeadTrackingUpdate();
+    }
+
+    void HeadTrackingUpdate()
+    {
+        // Store the current head rotation since we will be resetting it
+        Quaternion currentLocalRotation = headBone.localRotation;
+        // Reset the head rotation so our world to local space transformation will use the head's zero rotation. 
+        // Note: Quaternion.Identity is the quaternion equivalent of "zero"
+        headBone.localRotation = Quaternion.identity;
+
+        Vector3 targetWorldLookDir = target.position - headBone.position;
+        Vector3 targetLocalLookDir = headBone.InverseTransformDirection(targetWorldLookDir);
+
+        // Apply angle limit
+        targetLocalLookDir = Vector3.RotateTowards(
+            Vector3.forward,
+            targetLocalLookDir,
+            Mathf.Deg2Rad * headMaxTurnAngle, // Note we multiply by Mathf.Deg2Rad here to convert degrees to radians
+            0 // We don't care about the length here, so we leave it at zero
+        );
+
+        // Get the local rotation by using LookRotation on a local directional vector
+        Quaternion targetLocalRotation = Quaternion.LookRotation(targetLocalLookDir, Vector3.up);
+
+        // Apply smoothing
+        // More info: http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+        headBone.localRotation = Quaternion.Slerp(
+            currentLocalRotation,
+            targetLocalRotation,
+            1 - Mathf.Exp(-headTrackingSpeed * Time.deltaTime)
+        );
+    }
+    void RootMotionUpdate()
+    {
+        // Get the direction toward our target
+        Vector3 towardTarget = target.position - transform.position;
+        // Vector toward target on the local XZ plane
+        Vector3 towardTargetProjected = Vector3.ProjectOnPlane(towardTarget, transform.up);
+        // Get the angle from the gecko's forward direction to the direction toward toward our target
+        // Here we get the signed angle around the up vector so we know which direction to turn in
+        float angToTarget = Vector3.SignedAngle(transform.forward, towardTargetProjected, transform.up);
+
+        float targetAngularVelocity = 0;
+
+        // If we are within the max angle (i.e. approximately facing the target)
+        // leave the target angular velocity at zero
+        if (Mathf.Abs(angToTarget) > maxAngToTarget)
+        {
+            // Angles in Unity are clockwise, so a positive angle here means to our right
+            if (angToTarget > 0)
+            {
+                targetAngularVelocity = turnSpeed;
+            }
+            // Invert angular speed if target is to our left
+            else
+            {
+                targetAngularVelocity = -turnSpeed;
+            }
+        }
+
+        // Use our smoothing function to gradually change the velocity
+        currentAngularVelocity = Mathf.Lerp(
+            currentAngularVelocity,
+            targetAngularVelocity,
+            1 - Mathf.Exp(-turnAcceleration * Time.deltaTime)
+        );
+
+        // Rotate the transform around the Y axis in world space, 
+        // making sure to multiply by delta time to get a consistent angular velocity
+        transform.Rotate(0, Time.deltaTime * currentAngularVelocity, 0, Space.World);
+
+
+        Vector3 targetVelocity = Vector3.zero;
+
+        // Don't move if we're facing away from the target, just rotate in place
+        if (Mathf.Abs(angToTarget) < 90)
+        {
+            float distToTarget = Vector3.Distance(transform.position, target.position);
+
+            // If we're too far away, approach the target
+            if (distToTarget > maxDistToTarget)
+            {
+                targetVelocity = moveSpeed * towardTargetProjected.normalized;
+            }
+            // If we're too close, reverse the direction and move away
+            else if (distToTarget < minDistToTarget)
+            {
+                targetVelocity = moveSpeed * -towardTargetProjected.normalized;
+            }
+        }
+
+        currentVelocity = Vector3.Lerp(
+            currentVelocity,
+            targetVelocity,
+            1 - Mathf.Exp(-moveAcceleration * Time.deltaTime)
+        );
+
+        // Apply the velocity
+        transform.position += currentVelocity * Time.deltaTime;
+    }
+
+    IEnumerator LegUpdateCoroutine()
+    {
+        while (true)
+        {
+            do
+            {
+                frontLeftLegStepper.TryMove();
+                backRightLegStepper.TryMove();
+                yield return null;
+            } while (backRightLegStepper.Moving || frontLeftLegStepper.Moving);
+
+            do
+            {
+                frontRightLegStepper.TryMove();
+                backLeftLegStepper.TryMove();
+                yield return null;
+            } while (backLeftLegStepper.Moving || frontRightLegStepper.Moving);
+        }
+    }
+}
